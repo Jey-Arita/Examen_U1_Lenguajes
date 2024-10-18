c00526c4feeb13836e99b3043a106598ec3e2cb4
MichaelGald <michaelcom292@gmail.com>
2024-10-18 00:30:16 -0600

    primer_paso_swagger

--- Examen_U1_Lenguajes/Controllers/SolicitudPermisoController.cs
+++ Examen_U1_Lenguajes/Controllers/SolicitudPermisoController.cs
@@ -1,5 +1,6 @@
 ﻿using Examen_U1_Lenguajes.Dtos.Common;
 using Examen_U1_Lenguajes.Dtos.SolicitudPermisoDto;
+using Examen_U1_Lenguajes.Services;
 using Examen_U1_Lenguajes.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -21,13 +22,33 @@ namespace Examen_U1_Lenguajes.Controllers
         {
             var response = await _permisoService.GetSolicitudPermisoListAsync();
 
-            return StatusCode(response.StatusCode, new
-            {
-                response.Status,
-                response.Message,
-                response.Data
-            });
+            return StatusCode(response.StatusCode, response);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetSolicitudById(Guid id)
+        {
+            var response = await _permisoService.GetPermisoServiceByIdAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreateSolicitud(SolicitudPermisoCreateDto solicitudDto)
+        {
+            var response = await _permisoService.CreateAsync(solicitudDto);
+            return StatusCode(response.StatusCode, response);
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSolicitud(Guid id, RechazarOAprobarSolicitudesEditDto solicitudDto)
+        {
+            var response = await _permisoService.EditAsync(solicitudDto, id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSolicitud(Guid id)
+        {
+            var response = await _permisoService.DeleteAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
     }
 }
