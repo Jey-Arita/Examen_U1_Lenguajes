c921cd8c1a30389b4813e784afcd36579d44ca2a
MichaelGald <michaelcom292@gmail.com>
2024-10-18 00:03:41 -0600

    controller_proceso

---
+++ Examen_U1_Lenguajes/Controllers/SolicitudPermisoController.cs
@@ -0,0 +1,35 @@
+﻿using Examen_U1_Lenguajes.Dtos.Common;
+using Examen_U1_Lenguajes.Dtos.SolicitudPermisoDto;
+using Examen_U1_Lenguajes.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examen_U1_Lenguajes.Controllers
+{
+    [Route("api/solicitud")]
+    [ApiController]
+    public class SolicitudPermisoController : ControllerBase
+    {
+        private readonly ISolicitudPermisoService _permisoService;
+
+        public SolicitudPermisoController(ISolicitudPermisoService permisoService)
+        {
+            this._permisoService = permisoService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ResponseDto<SolicitudPermisoDto>>> GetAllSolicitudes()
+        {
+            var response = await _permisoService.GetSolicitudPermisoListAsync();
+
+            return StatusCode(response.StatusCode, new
+            {
+                response.Status,
+                response.Message,
+                response.Data
+            });
+        }
+
+        [HttpGet]
+        public async Task<>
+    }
+}
--- Examen_U1_Lenguajes/Services/SolicitudPermisoServices.cs
+++ Examen_U1_Lenguajes/Services/SolicitudPermisoServices.cs
@@ -1,34 +1,126 @@
-﻿using Examen_U1_Lenguajes.Dtos.Common;
+﻿using AutoMapper;
+using Examen_U1_Lenguajes.Database;
+using Examen_U1_Lenguajes.Database.Entities;
+using Examen_U1_Lenguajes.Dtos.Common;
 using Examen_U1_Lenguajes.Dtos.SolicitudPermisoDto;
 using Examen_U1_Lenguajes.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Net.WebSockets;
 
 namespace Examen_U1_Lenguajes.Services
 {
+    
     public class SolicitudPermisoServices : ISolicitudPermisoService
     {
-        public Task<ResponseDto<SolicitudPermisoDto>> CreateAsync(SolicitudPermisoCreateDto dto)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly Contexto _contexto;
+        private readonly IMapper _autoMapper;
 
-        public Task<ResponseDto<SolicitudPermisoDto>> DeleteAsync(Guid id)
+        public SolicitudPermisoServices(Contexto contexto, IMapper autoMapper)
         {
-            throw new NotImplementedException();
+            this._contexto = contexto;
+            this._autoMapper = autoMapper;
         }
-
-        public Task<ResponseDto<SolicitudPermisoDto>> EditAsync(RechazarOAprobarSolicitudesEditDto dto, Guid id)
+        public async Task<ResponseDto<List<SolicitudPermisoDto>>> GetSolicitudPermisoListAsync()
         {
-            throw new NotImplementedException();
+            var solicitud = await _contexto.SolicitudesPermiso.ToListAsync();
+            var solicitudDto = _autoMapper.Map<List<SolicitudPermisoDto>>(solicitud);
+            return new ResponseDto<List<SolicitudPermisoDto>>
+            {
+                StatusCode = 200,
+                Status = true,
+                Message = "Listado de registro obtenido correctamente.",
+                Data = solicitudDto
+            };
         }
-
-        public Task<ResponseDto<SolicitudPermisoDto>> GetPermisoServiceByIdAsync(Guid id)
+        public async Task<ResponseDto<SolicitudPermisoDto>> GetPermisoServiceByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var solicitudEntity = await _contexto.SolicitudesPermiso.FirstOrDefaultAsync();
+            if (solicitudEntity == null)
+            {
+                return new ResponseDto<SolicitudPermisoDto>
+                {
+                    StatusCode = 404,
+                    Status = false,
+                    Message = "No se encontro el registro"
+                };
+            }
+            var solicitudDto = _autoMapper.Map<SolicitudPermisoDto>(solicitudEntity);
+            return new ResponseDto<SolicitudPermisoDto>
+            {
+                StatusCode = 200,
+                Status = true,
+                Message = "Resgistro encontrado",
+                Data = solicitudDto
+            };
         }
-
-        public Task<ResponseDto<List<SolicitudPermisoDto>>> GetSolicitudPermisoListAsync()
+        public async Task<ResponseDto<SolicitudPermisoDto>> CreateAsync(SolicitudPermisoCreateDto dto)
         {
-            throw new NotImplementedException();
+            var solicituEntitry = _autoMapper.Map<SolicitudPermisoEntitty>(dto);
+            _contexto.SolicitudesPermiso.Add(solicituEntitry);
+            
+            await _contexto.SaveChangesAsync();
+           
+            var solicitudDto = _autoMapper.Map<SolicitudPermisoDto>(solicituEntitry);
+            return new ResponseDto<SolicitudPermisoDto>
+            {
+                StatusCode = 201,
+                Status = true,
+                Message = "Se a Creado una Solicitud",
+                Data = solicitudDto
+            };
+        }
+        public async Task<ResponseDto<SolicitudPermisoDto>> EditAsync(RechazarOAprobarSolicitudesEditDto dto, Guid id)
+        {
+            var solicitudEntity = await _contexto.SolicitudesPermiso.FirstOrDefaultAsync(s => s.Solicitud == id);
+
+            if (solicitudEntity == null)
+            {
+                return new ResponseDto<SolicitudPermisoDto>
+                {
+                    StatusCode = 404,
+                    Status = false,
+                    Message = "Solicitud no encontrada."
+                };
+            }
+
+            solicitudEntity.Estado = dto.Estado;
+            solicitudEntity.Observacion = dto.Observacion;
+
+            _contexto.SolicitudesPermiso.Update(solicitudEntity);
+            await _contexto.SaveChangesAsync();
+
+            var solicitudDto = _autoMapper.Map<SolicitudPermisoDto>(solicitudEntity);
+
+            return new ResponseDto<SolicitudPermisoDto>
+            {
+                StatusCode = 200,
+                Status = true,
+                Message = "Solicitud actualizada correctamente.",
+                Data = solicitudDto
+            };
+        }
+        public async Task<ResponseDto<SolicitudPermisoDto>> DeleteAsync(Guid id)
+        {
+            var solicitudEntity = await _contexto.SolicitudesPermiso.FirstOrDefaultAsync(s => s.Solicitud == id);
+
+            if (solicitudEntity == null)
+            {
+                return new ResponseDto<SolicitudPermisoDto>
+                {
+                    StatusCode = 404,
+                    Status = false,
+                    Message = "Solicitud no encontrada."
+                };
+            }
+            _contexto.SolicitudesPermiso.Remove(solicitudEntity);
+            await _contexto.SaveChangesAsync();
+
+            return new ResponseDto<SolicitudPermisoDto>
+            {
+                StatusCode = 200,
+                Status = true,
+                Message = "Solicitud eliminada correctamente."
+            };
         }
     }
 }
