66f865e034783344dcc6c6245d2910c5bd0e3dbe
Jey-Arita <jomaarita@gmail.com>
2024-10-17 19:21:24 -0600

    UsuaruisEntity-completado

--- Examen_U1_Lenguajes/Database/Entities/UsuarioEntity.cs
+++ Examen_U1_Lenguajes/Database/Entities/UsuarioEntity.cs
@@ -21,17 +21,21 @@ namespace Examen_U1_Lenguajes.Database.Entities
         [Column("fecha_ingreso")]
         public DateTime Fecha_ingreso { get; set; }
 
-        [Display(Name = "Telefono")]
-        [Required(ErrorMessage = "El {0} del empleado es requerido.")]
         [StringLength(80)]
         [Column("telefono")]
         public string Telefono { get; set; }
 
-        [Display(Name = "Telefono")]
+        [Display(Name = "Email")]
         [Required(ErrorMessage = "El {0} del empleado es requerido.")]
-        [StringLength(80)]
-        [Column("telefono")]
-        public string Telefono { get; set; }
+        [StringLength(320)]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "El {0} del empleado es requerido.")]
+        [StringLength(320)]
+        [Column("password")]
+        public string Password { get; set; }
 
     }
 }
