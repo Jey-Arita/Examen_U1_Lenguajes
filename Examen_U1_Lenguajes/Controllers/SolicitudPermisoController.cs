5b781ff06b8c27f6f3b8b30f7a35819996b663d0
MichaelGald <michaelcom292@gmail.com>
2024-10-18 03:12:47 -0600

    subir_bruno

--- Examen_U1_Lenguajes/Controllers/SolicitudPermisoController.cs
+++ Examen_U1_Lenguajes/Controllers/SolicitudPermisoController.cs
@@ -1,4 +1,5 @@
-﻿using Examen_U1_Lenguajes.Dtos.Common;
+﻿using Examen_U1_Lenguajes.Constants;
+using Examen_U1_Lenguajes.Dtos.Common;
 using Examen_U1_Lenguajes.Dtos.SolicitudPermisoDto;
 using Examen_U1_Lenguajes.Services;
 using Examen_U1_Lenguajes.Services.Interfaces;
@@ -17,20 +18,26 @@ namespace Examen_U1_Lenguajes.Controllers
         {
             this._permisoService = permisoService;
         }
+
         [HttpGet]
+        [Authorize(Roles = $"{RolesConstant.ADMIN}")]
         public async Task<ActionResult<ResponseDto<SolicitudPermisoDto>>> GetAllSolicitudes()
         {
             var response = await _permisoService.GetSolicitudPermisoListAsync();
 
             return StatusCode(response.StatusCode, response);
         }
+
         [HttpGet("{id}")]
+        [Authorize(Roles = $"{RolesConstant.ADMIN}, {RolesConstant.EMPLEADO}")]
         public async Task<ActionResult> GetSolicitudById(Guid id)
         {
             var response = await _permisoService.GetPermisoServiceByIdAsync(id);
             return StatusCode(response.StatusCode, response);
         }
+
         [HttpPost]
+        [Authorize(Roles = $"{RolesConstant.EMPLEADO}")]
         public async Task<ActionResult> CreateSolicitud(SolicitudPermisoCreateDto solicitudDto)
         {
             var response = await _permisoService.CreateAsync(solicitudDto);
@@ -38,6 +45,7 @@ namespace Examen_U1_Lenguajes.Controllers
         }
 
         [HttpPut("{id}")]
+        [Authorize(Roles = $"{RolesConstant.ADMIN}")]
         public async Task<IActionResult> UpdateSolicitud(Guid id, RechazarOAprobarSolicitudesEditDto solicitudDto)
         {
             var response = await _permisoService.EditAsync(solicitudDto, id);
@@ -45,6 +53,7 @@ namespace Examen_U1_Lenguajes.Controllers
         }
 
         [HttpDelete("{id}")]
+        [Authorize(Roles = $"{RolesConstant.ADMIN}")]
         public async Task<IActionResult> DeleteSolicitud(Guid id)
         {
             var response = await _permisoService.DeleteAsync(id);
